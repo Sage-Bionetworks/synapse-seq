fbba2a1cc85621965b4124cf389101053ccadca3
kkdang <kristen.dang@sagebase.org>
2014-03-21 17:06:35 -0700

    Generalized to accept synID as argument.

--- scripts/eval_listener.py
+++ scripts/eval_listener.py
@@ -1,27 +1,25 @@
 #! /usr/bin/env python
 # Dec. 9, 2013
 # KKD for Sage Bionetworks
+# Requires: synapseseq installed such that contents of scripts folder are callable on command line
+# To do: record version of synapseseq in provenance
 
-
-import synapseclient, os, yaml, argparse
+import synapseclient, os, yaml, argparse, synapseseq
 import seq_running as sr
 
 parser = argparse.ArgumentParser(description='Checks for any new submissions to seq workflow evaluations and runs the corrsponding compute jobs.')
-parser.add_argument('--local', dest='bin', required=False, help='Location for code files on this compute system.', default='~/bin/scripts')
+parser.add_argument('--evals', dest='synid', required=False, help='Synapse ID of yaml-format file containing evaluation ids and corresponding script names.', default='syn2319132')
 args = parser.parse_args()
 
 syn = synapseclient.Synapse()
 syn.login()
 profile = syn.getUserProfile() 
-evalListFile = syn.get('syn2319132', downloadFile = True) # yaml file
+evalListFile = syn.get(args.synid, downloadFile = True) # yaml file
 
 stream = open(evalListFile.path, 'r')
 for eval,code in yaml.load(stream).iteritems():
-	print 'eval %s: code file: %s' % (eval, code)
+	print 'Checking submissions for eval %s: %s' % (eval, code)
 	
-	# Get code from local install
-	codePath = os.path.join(args.bin, code)
-
 	# Make location for logs 
 	logsDir = os.path.join(os.getcwd(), 'logs')
 	if not os.path.exists(logsDir):
@@ -31,6 +29,9 @@ for eval,code in yaml.load(stream).iteritems():
 	for submission in syn.getSubmissions(eval):	
 #		print '%s' % submission.entityId
 		submissionAnnotations = syn.getAnnotations(submission.entityId, version=submission.versionNumber)
-		sr.runJobsForSubmissions(submission, codePath, logsDir, outputProjectID=submissionAnnotations['outputID'], commandLineParams=submissionAnnotations['commandLineParams'], externalBucket=submissionAnnotations['bucket'], syn=syn)
+		
+		if 'bucket' in submissionAnnotations:
+			sr.runJobsForSubmissions(submission, code, logsDir, outputProjectID=submissionAnnotations['outputID'], commandLineParams=submissionAnnotations['workflowParams'], externalBucket=submissionAnnotations['bucket'], syn=syn)
 
-
+		else:
+			sr.runJobsForSubmissions(submission, code, logsDir, outputProjectID=submissionAnnotations['outputID'], commandLineParams=submissionAnnotations['workflowParams'], syn=syn)
