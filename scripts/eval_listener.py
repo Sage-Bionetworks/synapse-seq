73615bd19adc43108995af4c33aac596db4fb3f9
kkdang <kristen.dang@sagebase.org>
2014-04-01 10:36:28 -0700

    Takes command-line params as synID, uses client directly to submit.

--- scripts/eval_listener.py
+++ scripts/eval_listener.py
@@ -1,37 +1,71 @@
 #! /usr/bin/env python
 # Dec. 9, 2013
 # KKD for Sage Bionetworks
-# Requires: synapseseq installed such that contents of scripts folder are callable on command line
-# To do: record version of synapseseq in provenance
 
-import synapseclient, os, yaml, argparse, synapseseq
-import seq_running as sr
+# Hardcoded: path to python in qsub statement.
+# To do: get reference
+
+import synapseclient, os, yaml, argparse, subprocess
 
 parser = argparse.ArgumentParser(description='Checks for any new submissions to seq workflow evaluations and runs the corrsponding compute jobs.')
-parser.add_argument('--evals', dest='synid', required=False, help='Synapse ID of yaml-format file containing evaluation ids and corresponding script names.', default='syn2319132')
+parser.add_argument('--evals', dest='synid', required=False, help='Synapse ID of yaml-format file containing evaluation ids and corresponding script names.')
+parser.add_argument('--synapseseq', dest='bin', required=False, help='Path to installation location of scripts files from synapseseq package.', default='/usr/local/somewhere')
 args = parser.parse_args()
 
-syn = synapseclient.Synapse()
-syn.login()
+syn = synapseclient.login()
 profile = syn.getUserProfile() 
 evalListFile = syn.get(args.synid, downloadFile = True) # yaml file
 
+
+def parseJobParameters(synapseID):
+	yamlFileEntity = syn.get(synapseID)
+	stream = open(yamlFileEntity.path, 'r')
+	
+	jobParams = ''
+	for params in yaml.load(stream).iteritems():
+		for item in params:
+			jobParams += ' '+str(item)
+	jobParams += ' --params '+synapseID
+	print 'Read these params: %s' % jobParams
+	return(jobParams)
+
+paramFiles = dict()
+
 stream = open(evalListFile.path, 'r')
 for eval,code in yaml.load(stream).iteritems():
-	print 'Checking submissions for eval %s: %s' % (eval, code)
+	print 'eval %s: code file: %s' % (eval, code)
 	
-	# Make location for logs 
+	codePath = os.path.join(args.bin, code)
+
 	logsDir = os.path.join(os.getcwd(), 'logs')
 	if not os.path.exists(logsDir):
 		os.mkdir(logsDir)
 
-	# Run count job for open submissions
+	# Run jobs for open submissions
 	for submission in syn.getSubmissions(eval):	
 #		print '%s' % submission.entityId
-		submissionAnnotations = syn.getAnnotations(submission.entityId, version=submission.versionNumber)
-		
-		if 'bucket' in submissionAnnotations:
-			sr.runJobsForSubmissions(submission, code, logsDir, outputProjectID=submissionAnnotations['outputID'], commandLineParams=submissionAnnotations['workflowParams'], externalBucket=submissionAnnotations['bucket'], syn=syn)
+		status = syn.getSubmissionStatus(submission)
+		if status.status == 'RECEIVED':
 
-		else:
-			sr.runJobsForSubmissions(submission, code, logsDir, outputProjectID=submissionAnnotations['outputID'], commandLineParams=submissionAnnotations['workflowParams'], syn=syn)
+			additionalParams = ''
+			entityAnnotations = syn.getAnnotations(submission.entityId)
+			if 'bucket' in entityAnnotations:
+				additionalParams += '--bucket ' + entityAnnotations['bucket'][0]
+				additionalParams += ' --keyname ' + entityAnnotations['key'][0]
+			if 'eval_'+str(eval) in entityAnnotations: # Are there command line params in the entity's annotations?
+				evalName = 'eval_'+str(eval)
+				if entityAnnotations[evalName][0] not in paramFiles: # check whether this file has been parsed
+					additionalParams = parseJobParameters(entityAnnotations[evalName][0])
+					paramFiles[entityAnnotations[evalName][0]] = additionalParams
+				else:
+					additionalParams = paramFiles[entityAnnotations[evalName][0]]
+
+			cmd = ' '.join(['qsub -N synapseseq -o', os.path.join(logsDir, '$JOB_NAME.$JOB_ID'), '-j y -S /usr/bin/python -V', codePath, '--submission', submission.id, additionalParams])
+
+			print '%s' % cmd
+ 			subprocess.call(cmd, shell = True)
+# 			status.status = 'OPEN' 
+# 			status = syn.store(status)
+
+
+
