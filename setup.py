13947be95817060c624a56141e4436e7afdc7477
kkdang <kristen.dang@sagebase.org>
2014-10-01 15:45:20 -0400

    Fixed dependency list.

--- setup.py
+++ setup.py
@@ -8,5 +8,5 @@ setup(name='synapseSeq', version='0.1adev', author='Kristen Dang',
       license='LICENSE.txt',
       description='For running seq pipelines on [cloud|local] clusters using Synapse to track data and provenance.',
       long_description=open('README.md').read(),
-      install_requires=['synapseclient>0.5.0', 'os', 'argparse', 'sys', 'subprocess', 'hashlib', 'shutil'],)
+      install_requires=['synapseclient>0.5.0'],)
       
