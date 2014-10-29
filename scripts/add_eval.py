21a3c2b2b2901c5a6f525ab880d049d42269e44c
kkdang <kristen.dang@sagebase.org>
2014-10-29 16:42:56 -0700

    Added main().

--- scripts/add_eval.py
+++ scripts/add_eval.py
@@ -1,23 +1,26 @@
 #! /usr/bin/env python
 # KKD for Sage Bionetworks
-# Create synapse evaluation entity
 
 import synapseclient, argparse
 from synapseclient import Evaluation
 
-parser = argparse.ArgumentParser(description='Creates XYZ evaluation via synapse.')
-parser.add_argument('--name', dest='name', required=True, help='Name of workflow')
-parser.add_argument('--description', dest='desc', required=True, help='Description of workflow')
-parser.add_argument('--parent', dest='pid', required=False, help='Project ID with which to associate this evaluation.', default='syn1972151')
+def main():
+	parser = argparse.ArgumentParser(description='Creates XYZ evaluation via synapse.')
+	parser.add_argument('--name', dest='name', required=True, help='Name of workflow')
+	parser.add_argument('--description', dest='desc', required=True, help='Description of workflow')
+	parser.add_argument('--parent', dest='pid', required=False, help='Project ID with which to associate this evaluation.', default='syn1972151')
 
-args = parser.parse_args()
+	args = parser.parse_args()
 
 
-syn = synapseclient.Synapse()
-syn.login()
+	syn = synapseclient.Synapse()
+	syn.login()
 
-newEval = Evaluation(name = args.name, description = args.desc, contentSource = args.pid)
-newEval = syn.store(newEval)
-syn.joinEvaluation(newEval)
+	newEval = Evaluation(name = args.name, description = args.desc, contentSource = args.pid)
+	newEval = syn.store(newEval)
+	syn.joinEvaluation(newEval)
 
-print 'Synapse ID for evaluation %s is %s' % (args.name, newEval.id)
+	print 'Synapse ID for evaluation %s is %s' % (args.name, newEval.id)
+
+
+if __name__ == '__main__': main()
