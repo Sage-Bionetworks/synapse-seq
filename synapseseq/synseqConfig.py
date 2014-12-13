b267b062669384cb7b1e1552583b82cb41900833
Kristen Dang <kristen.dang@sagebase.org>
2014-12-13 01:20:20 +0000

    Moved some sys config details to config file.

---
+++ synapseseq/synseqConfig.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python
+
+
+workerUserName = 'ubuntu'
+sailfishPath = '/home/ubuntu/bin/Sailfish-0.6.3-Linux_x86-64'
+picard = '/usr/local/share/java/picard-1.96/'
+headNFSPath = '/home/cbl_data/'
+localWDPath = '/mnt/' # Execution node wd on cloudbiolinux
+
+sailfishMetricsTable = 'syn2907457'
