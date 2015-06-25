37c06b3237960242d3c6b7b7031adec90380f5fd
kkdang <kristen.dang@sagebase.org>
2015-06-25 08:57:37 -0700

    Starting to use expanded config file.

--- scripts/eval_align_star.py
+++ scripts/eval_align_star.py
@@ -10,26 +10,30 @@ import synapseclient, synapseseq
 from synapseclient import Activity, File
 #from synapseclient.table import Table
 from synapseseq import seq_running as sr
-from synapseseq import synseqConfig as syncfg
 
 
 parser = argparse.ArgumentParser(description='Aligns reads in input BAM using rna-star.')
 parser.add_argument('--submission', required=True, dest='bam', help='Submission id of BAM file to process.')
-parser.add_argument('--params', required=True, help='Synapse ID of the parameter file for this job.')
-parser.add_argument('--ref', dest='ref', required=True, help='Synapse ID of reference genome used for alignment.', default=None)
-parser.add_argument('--output', dest='out', required=True, help='Synapse ID of project or folder to contain the output data.')
-parser.add_argument('--threads', dest='threads', required=True, help='Number cpus to use per job')
+parser.add_argument('--params', required=False, help='Synapse ID of the parameter file for this job. Required if not specified in config file.')
+parser.add_argument('--config', dest='conf', required=False, help='Path to local config file. Required.')
+parser.add_argument('--ref', dest='ref', required=False, help='Synapse ID of reference genome used for alignment. Required if not specified in config file.', default=None)
+parser.add_argument('--output', dest='out', required=False, help='Synapse ID of project or folder to contain the output data. Required if not specified in config file.')
+parser.add_argument('--threads', dest='threads', required=False, help='Number cpus to use per job.', default=4)
+parser.add_argument('--toFollow', dest='follow', required=False, help='Synapse ID of evaluation to run on the data following this one.', default=None)
 parser.add_argument('--bucket', dest='bucket', required=False, help='Bucket name if not stored in Synapse.', default=None)
 parser.add_argument('--keyname', dest='key', required=False, help='Key name if not stored in Synapse.', default=None)
 args = parser.parse_args()
 
 
-subprocess.call(' '.join(['sudo chown ubuntu:ubuntu', syncfg.localWDPath]), shell = True)
-sr.getSynConfigFromHead(localPath='/home/'+syncfg.workerUserName+'/',headPath=syncfg.headNFSPath)
+config = yaml.load(file(args.conf))
+
+
+subprocess.call(' '.join(['sudo chown ubuntu:ubuntu', config['localWDPath']]), shell = True)
+sr.getSynConfigFromHead(localPath='/home/'+config['workerUserName']+'/',headPath=config['headNFSPath'])
 
 ## Set up output directory, log in to synapse	
 evalName = 'ALIGNstar'
-wd = os.path.join(syncfg.localWDPath, evalName)
+wd = os.path.join(config['localWDPath'], evalName)
 if not os.path.exists(wd):
 	os.mkdir(wd)
 if not os.path.exists(os.path.join(wd, 'tmp')):
@@ -39,8 +43,7 @@ syn.login()
 
 
 ## Get index
-#index = sr.copyRefToWorkerNode(args.idx,syncfg.headNFSPath,wd, syn)
-index = os.path.join(syncfg.headNFSPath, syncfg.GRCh38_star_RL100)
+index = os.path.join(config['headNFSPath'], config['GRCh38_star_RL100'])
 
 
 ## Get submission
@@ -62,7 +65,7 @@ R2file = os.path.join(wd, prefix + '_R2.fastq')
 outputFile = os.path.join(wd, prefix + '.samtofastq')
 
 
-cmd = ' '.join(['java -Xmx2G -jar', os.path.join(syncfg.picard, 'SortSam.jar'), 'INPUT=', localBAMfilePath, 'OUTPUT=/dev/stdout SORT_ORDER=queryname QUIET=true VALIDATION_STRINGENCY=SILENT COMPRESSION_LEVEL=0 TMP_DIR=', os.path.join(wd, 'tmp'), '| java -Xmx2G -jar', os.path.join(syncfg.picard, 'SamToFastq.jar'), 'INPUT=/dev/stdin FASTQ=', R1file, 'SECOND_END_FASTQ=', R2file, 'TMP_DIR=', os.path.join(wd, 'tmp'), 'VALIDATION_STRINGENCY=SILENT'])
+cmd = ' '.join(['java -Xmx2G -jar', os.path.join(config['picard'], 'SortSam.jar'), 'INPUT=', localBAMfilePath, 'OUTPUT=/dev/stdout SORT_ORDER=queryname QUIET=true VALIDATION_STRINGENCY=SILENT COMPRESSION_LEVEL=0 TMP_DIR=', os.path.join(wd, 'tmp'), '| java -Xmx2G -jar', os.path.join(config['picard'], 'SamToFastq.jar'), 'INPUT=/dev/stdin FASTQ=', R1file, 'SECOND_END_FASTQ=', R2file, 'TMP_DIR=', os.path.join(wd, 'tmp'), 'VALIDATION_STRINGENCY=SILENT'])
 
 if not os.path.exists(R1file) and not os.path.exists(R2file):
 	print 'samtofastq start %s' % time.asctime()
@@ -106,7 +109,7 @@ print 'new entity id %s' % alignEntity.id
 
 ## Use this code after related JIRA is resolved
 ## Load metrics to Synapse table
-# table = syn.get(syncfg.sailfishMetricsTable)
+# table = syn.get(config['sailfishMetricsTable'])
 # metrics = list()
 # metrics.append(prefix)
 # with open(os.path.join(wd, prefix+'_count_info.sf')) as metricsFile:	
@@ -127,3 +130,15 @@ shutil.rmtree(wd)
 # status = syn.getSubmissionStatus(submission)
 # status.status = 'SCORED' # Scored is functioning as "finished" for now.
 # status = syn.store(status)
+
+
+# Submit any following jobs and run
+followingSubmission = syn.submit(entity=aligneEntity, evaluation = args.follow,  name = alignEntity.name, teamName = submission.teamName)
+print 'Submitted %s to %s' % (aligneEntity.name, args.follow)
+
+cmd = ' '.join([codePath, '--submission', followingSubmission.id, additionalParams])
+print '%s' % cmd
+subprocess.call(cmd, shell = True)
+
+
+
